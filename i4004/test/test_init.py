96fb959701fadd49a9cf31b54196270b9136faff
alshapton <alshapton@gmail.com>
2021-03-03 22:19:24 +0000

    Updated test script

--- i4004/test/test_init.py
+++ i4004/test/test_init.py
@@ -1,21 +1,13 @@
-# Using pytest and delayed-assert add-in
-import delayed_assert
+# Using pytest
 from i4004.src.i4004 import processor
 
 def test_init():
-    delayed_assert.expect(chip.read_accumulator() == 0)
-    delayed_assert.expect(chip.MAX_4_BITS == 15)
-    delayed_assert.expect(chip.read_carry() is False)
-    delayed_assert.expect(sum(chip.read_all_ram()) == 0)
-    delayed_assert.expect(sum(chip.read_all_rom()) == 0)
-    delayed_assert.expect(sum(chip.read_all_registers()) == 0)
-   
-    
-    # delayed_assert.expect(sum(chip.read_all_pram()) == 0) # Needs work
-    
-   
-    delayed_assert.assert_expectations()
-
+    assert(chip.read_accumulator() == 0)
+    assert(chip.MAX_4_BITS == 15)
+    assert(chip.read_carry() is False)
+    assert(sum(chip.read_all_ram()) == 0)
+    assert(sum(chip.read_all_rom()) == 0)
+    assert(sum(chip.read_all_registers()) == 0)
 
 chip = processor()
 
