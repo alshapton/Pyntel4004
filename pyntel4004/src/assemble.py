f4768719a853fd52c868e05c23a5b0dc63f797ed
alshapton <alshapton@gmail.com>
2021-06-25 23:54:26 +0100

    Commenced separating assembler from emulator

--- pyntel4004/src/assemble.py
+++ pyntel4004/src/assemble.py
@@ -5,6 +5,9 @@ import ast
 import getopt
 import sys
 
+# Assembler imports
+from assembler.supporting import add_label
+
 ##############################################################################
 #  _ _  _    ___   ___  _  _     ______                 _       _            #
 # (_) || |  / _ \ / _ \| || |   |  ____|               | |     | |           #
@@ -287,44 +290,6 @@ def execute(chip: processor, location: str, PC: int, monitor: bool):
 # |_|  |_|  \___/ \___/   |_| /_/    \_\__/__/\__|_| |_| |_|_.__/|_|\___|_|   #
 #                                                                             #
 ###############################################################################
-
-
-def add_label(_L, label: str):
-    """
-    Add a label to the label table (if it does not exist already).
-
-    Parameters
-    ----------
-    _L : list, mandatory
-        A list of the existing labels
-    label: str, mandatory
-        A candidate new label
-
-    Returns
-    -------
-    -1          if the label already existed and was not added
-    _L : list
-                the list of labels with the new label appended
-
-    Raises
-    ------
-    N/A
-
-    Notes
-    ------
-    N/A
-
-    """
-    try:
-        label_exists = next((item for item in _L
-                            if str(item["label"]) == label), None)
-    except: # noqa
-        pass
-    if not label_exists:
-        _L.append({'label': label, 'address': -1})
-    else:
-        return -1
-    return _L
 
 
 def match_label(_L, label: str, address):
---
+++ pyntel4004/src/assembler/supporting.py
@@ -0,0 +1,36 @@
+def add_label(_L, label: str):
+    """
+    Add a label to the label table (if it does not exist already).
+
+    Parameters
+    ----------
+    _L : list, mandatory
+        A list of the existing labels
+    label: str, mandatory
+        A candidate new label
+
+    Returns
+    -------
+    -1          if the label already existed and was not added
+    _L : list
+                the list of labels with the new label appended
+
+    Raises
+    ------
+    N/A
+
+    Notes
+    ------
+    N/A
+
+    """
+    try:
+        label_exists = next((item for item in _L
+                            if str(item["label"]) == label), None)
+    except: # noqa
+        pass
+    if not label_exists:
+        _L.append({'label': label, 'address': -1})
+    else:
+        return -1
+    return _L
