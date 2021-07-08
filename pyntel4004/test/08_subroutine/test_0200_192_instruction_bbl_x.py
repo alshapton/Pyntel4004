adb74b6e411988a78b402f21d27fecbfb364ed37
alshapton <alshapton@gmail.com>
2021-07-08 09:53:11 +0100

    Completed BBL (and therefore, subroutine) tests

--- pyntel4004/test/08_subroutine/test_0200_192_instruction_bbl_x.py
+++ pyntel4004/test/08_subroutine/test_0200_192_instruction_bbl_x.py
@@ -22,7 +22,7 @@ def test_validate_instruction(increment):
     assert op == known
 
 
-'''
+
 @pytest.mark.parametrize("value", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                                    12, 13, 14, 15])
 def test_bbl_scenario1(value):
@@ -30,10 +30,27 @@ def test_bbl_scenario1(value):
     chip_test = processor()
     chip_base = processor()
 
+    '''
+    chip_base is where I want the end conditions to be constructed by me
+    chip_test is where I want the start conditions to be managed by me, and the
+    bbl instruction to manipulate and be compared with chip_base
+
+    location 300
+    jms 400 (pc = 400) (1)
+            (pc = 302) (3) - end
+
+    location 400
+    bbl value (pc = 400) (2)
+
+    '''
+    PC = 300
+
     # Simulate conditions at end of instruction in base chip
     chip_base.PROGRAM_COUNTER = 300
     chip_base.write_to_stack(chip_base.PROGRAM_COUNTER + 2)
-    chip_base.PROGRAM_COUNTER = address12 - 1
+    chip_base.PROGRAM_COUNTER = 302  # Return
+    chip_base.ACCUMULATOR = value
+    chip_base.STACK_POINTER = 2
 
     # Set up conditions in test chip
     chip_test.PROGRAM_COUNTER = 300
@@ -41,35 +58,14 @@ def test_bbl_scenario1(value):
 
     # Perform the instruction under test:
     # Return from a subroutine and load a value into the accumulator
-    processor.bbl(chip_test, address12)
+    processor.bbl(chip_test, value)
 
     # Make assertions that the base chip is now at the same state as
     # the test chip which has been operated on by the instruction under test.
 
-    assert chip_test.PROGRAM_COUNTER == address12 - 1
-    assert chip_test.STACK_POINTER == 1
-    assert chip_test.STACK[chip_test.STACK_POINTER + 1] == 302  # Return
+    assert chip_test.PROGRAM_COUNTER == PC + 2
+    assert chip_test.STACK_POINTER == 2
+    assert chip_test.STACK[chip_test.STACK_POINTER] == 302  # Return
 
     # Pickling each chip and comparing will show equality or not.
     assert pickle.dumps(chip_test) == pickle.dumps(chip_base)
-
-
-@pytest.mark.parametrize("address12", [-1, 4096])
-def test_jms_scenario2(address12):
-    """Test JMS instruction failure."""
-    chip_test = processor()
-
-    # Simulate conditions at START of operation in base chip
-    # chip should have not had any changes as the operations will fail
-    # N/A
-
-    # Simulate conditions at END of operation in test chip
-    # chip should have not had any changes as the operations will fail
-    # N/A
-
-    # attempting to use an invalid value
-    with pytest.raises(Exception) as e:
-        assert processor.jms(chip_test, address12)
-    assert str(e.value) == ' Value: ' + str(address12)
-    assert e.type == ValueOutOfRangeForStack
-'''
