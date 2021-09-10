ddc7203c0d8a7a2d4471bf7309504da046c5bea0
alshapton <alshapton@gmail.com>
2021-09-10 02:10:52 +0100

    Completed test for RDn and added some documentation diagrams

---
+++ docs/source/hardware/machine/inc.rst
@@ -0,0 +1,79 @@
+.. _hardware-machine-inc:
+
+SRC
+===
+
+.. include:: ../../global.rst
+
+.. toctree::
+   :hidden:
+
+.. list-table::
+   :widths: 25 75
+   :header-rows: 0
+
+
+   * - Name
+     - Increment Register
+   * - Function
+     - Increments a specified register by 1.
+   * - Syntax
+     - INC(R)
+   * - Assembled
+     -
+   * - Binary
+     - 0110RRRR
+   * - Decimal
+     - 96, then incrementing by 1 until 111
+   * - Hexadecimal
+     - 0x60, then incrementing by 1 until 0x6F
+   * - Symbolic
+     - .. image:: images/inc-sym.png
+          :scale: 50%
+   * - Execution
+     - 1 word, 8-bit code and an execution time of 10.3 |mu| sec
+   * - Side-effects
+     - Not Applicable
+   * - Implemented
+     - inc_
+
+.. rubric:: Detailed Description
+
+The address contained within the specified register pair designates either
+a particular DATA RAM data character, a DATA RAM status character,
+a RAM output port, or a ROM input/output port.
+However, the address designates all of these simultaneously; it is up to
+the programmer to then write the correct I/O or RAM instruction to access
+the proper entity.
+
+The disassembly of the instruction below shows how the register pair are
+represented in the opcode.
+
+.. image:: images/src.png
+   :scale: 50%
+   :align: center
+
+The register specified in the lower 4 bits of the instruction is incremented by 1. 
+The carry bit will remain unchanged. If the register specified contains a value of 0b1111
+and an INC instruction is applied, the register will contain a value of 0b0000, but
+the carry bit will remain unchanged
+
+
+.. rubric:: Example program
+
+::
+
+    / Example program
+    / Loads the Accumulator with a value of 2
+    / places that value in Register 6
+    / increments Register 6
+    / Register 6 contains a value of 3
+            org    ram
+            ld     2
+            xch    6
+            inc    6
+            end
+
+
+
+.. _src: https://github.com/alshapton/Pyntel4004/blob/5e9f4253d8a412f6a3ec8fca5e3acfc88e0861c3/pyntel4004/src/hardware/machine.py#L208
---
+++ pyntel4004/test/06_io_ram/test_0200_236_instruction_rdn_x.py
@@ -0,0 +1,75 @@
+# Using pytest
+# Test the RDn instructions of an instance of an i4004(processor)
+
+import sys
+import pickle
+import pytest
+sys.path.insert(1, '../src')
+
+from hardware.processor import processor  # noqa
+from hardware.exceptions import InvalidRamBank  # noqa
+from hardware.suboperation import insert_registerpair 
+
+def test_validate_rdN_instruction():
+    """Ensure instruction's characteristics are valid."""
+    chip_test = processor()
+    # Validate the instruction's opcode and characteristics:
+    op = chip_test.INSTRUCTIONS[236]
+    known = {"opcode": 236, "mnemonic": "rd0()", "exe": 10.8, "bits": ["1110", '1100'], "words": 1}  # noqa
+    assert op == known
+    
+    op = chip_test.INSTRUCTIONS[237]
+    known = {"opcode": 237, "mnemonic": "rd1()", "exe": 10.8, "bits": ["1110", '1101'], "words": 1}  # noqa
+    assert op == known
+    
+    op = chip_test.INSTRUCTIONS[238]
+    known = {"opcode": 238, "mnemonic": "rd2()", "exe": 10.8, "bits": ["1110", '1110'], "words": 1}  # noqa
+    assert op == known
+    
+    op = chip_test.INSTRUCTIONS[239]
+    known = {"opcode": 239, "mnemonic": "rd3()", "exe": 10.8, "bits": ["1110", '1111'], "words": 1}  # noqa
+    assert op == known
+
+@pytest.mark.parametrize("chip", [0, 1, 2, 3])
+@pytest.mark.parametrize("register", [0, 1, 2, 3]) 
+def test_rdN_scenario1(chip, register):
+
+    from random import seed
+    from random import randint
+
+    """Test RDM instruction functionality."""
+    chip_test = processor()
+    chip_base = processor()
+
+    value = randint(0,15)
+    
+    address = (chip << 6) + (register << 4)
+    chip_test.COMMAND_REGISTER = address
+    chip_test.CURRENT_RAM_BANK = 0
+    chip_test.STATUS_CHARACTERS[chip_test.CURRENT_RAM_BANK][chip][register][register] = value
+
+    # Perform the instruction under test:
+    if register == 0:
+        processor.rd0(chip_test)
+    if register == 1:
+        processor.rd1(chip_test)
+    if register == 2:
+        processor.rd2(chip_test)
+    if register == 3:
+        processor.rd3(chip_test)
+    
+    # Simulate conditions at end of instruction in base chip
+    chip_base.COMMAND_REGISTER = address
+    chip_base.increment_pc(1)
+    chip_base.set_accumulator(value)
+    chip_base.STATUS_CHARACTERS[chip_test.CURRENT_RAM_BANK][chip][register][register] = value
+
+    
+    # Make assertions that the base chip is now at the same state as
+    # the test chip which has been operated on by the instruction under test.
+
+    assert chip_test.read_program_counter() == chip_base.read_program_counter()
+    assert chip_test.read_accumulator() == chip_base.read_accumulator()
+
+    # Pickling each chip and comparing will show equality or not.
+    assert pickle.dumps(chip_test) == pickle.dumps(chip_base)
