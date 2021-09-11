163a427559f5050a081de291294a30226e37ce24
alshapton <alshapton@gmail.com>
2021-09-11 23:12:15 +0100

    Added test for missing opcode 254

--- pyntel4004/test/01_startup/test_0160_empty_opcodes.py
+++ pyntel4004/test/01_startup/test_0160_empty_opcodes.py
@@ -9,7 +9,7 @@ from hardware.processor import processor  # noqa
 
 
 @pytest.mark.parametrize("opcode", [1, 2, 3, 4, 5, 6, 7,
-                                    8, 9, 10, 11, 12, 13, 14, 15])
+                                    8, 9, 10, 11, 12, 13, 14, 15, 254])
 def test_validate_instruction1_15(opcode):
     """Validate the instruction's opcode and characteristics."""
     chip_test = processor()
